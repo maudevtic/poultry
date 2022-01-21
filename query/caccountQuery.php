3ea5b2c2e9971e878cb9f45217a01bb0dc27dc95
maudev <maudevtic@gmail.com>
2022-01-21 13:42:32 +0800

    partial create account

--- query/caccountQuery.php
+++ query/caccountQuery.php
@@ -1,10 +1,12 @@
 <?php
 	include "connection.php";
 
-	if (isset($_POST['loginBtn'])) {
+	if (isset($_POST['caccountBtn'])) {
 		$user = $_POST['username'];
 		$pass = $_POST['password'];
+    $type = $_POST['type'];
 		$sql = mysqli_query($con, "SELECT * FROM users WHERE username = '$user' AND password = '$pass'");
+    $sql = mysqli_query($con, "INSERT INTO users (username, password, type) VALUES ('$user', '$pass', '$type')");
 		$row = mysqli_num_rows($sql);
 		if ($row>=1) {
 			session_start();
